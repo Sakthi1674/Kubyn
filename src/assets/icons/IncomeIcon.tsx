d93131fe8c8ceaf69b9c7d5ae703a690d60d8386
Gayathrimanikindi <gayathrimanikindi@gmail.com>
2025-10-28 17:51:52 +0530

    28/10/25

---
+++ src/assets/icons/IncomeIcon.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react';
+import Svg, { Path } from 'react-native-svg';
+
+const IncomeIcon = ({ width = 32, height = 32, color = '#FBFDFF' }) => (
+  <Svg
+    width={width}
+    height={height}
+    viewBox="0 0 32 32"
+    fill="none"
+  >
+    <Path
+      d="M29.3327 15.9998C29.3327 23.3598 23.3593 29.3332 15.9993 29.3332C8.63935 29.3332 2.66602 23.3598 2.66602 15.9998C2.66602 8.63984 8.63935 2.6665 15.9993 2.6665"
+      stroke={color}
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M22.666 4V9.33333H27.9993"
+      stroke={color}
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M29.3327 2.6665L22.666 9.33317"
+      stroke={color}
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+    <Path
+      d="M12.5 11.75H19.5M12.5 14.6667H19.5M17.4583 22.25L12.5 17.5833H14.25C18.1391 17.5833 18.1391 11.75 14.25 11.75"
+      stroke={color}
+      strokeWidth={2}
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    />
+  </Svg>
+);
+
+export default IncomeIcon;
